1f1b6f4b7061a30d90b6441e502fc7618729ec78
Nhan Le <nhanhl1@uci.edu>
2026-08-20 20:02:07 -0700

    added /join_room endpoint to backend, going to connect with frontend next

--- server/app/routes/rooms.py
+++ server/app/routes/rooms.py
@@ -16,3 +16,22 @@ def make_room(session_id: Annotated[str | None, Cookie()] = None):
     user_id = session["session_userid"]
     room_id = create_room(user_id)
     return {"room_id": room_id}
+
+@router.post("/join_room/{room_id}")
+def join_room(room_id: str, session_id: Annotated[str | None, Cookie()] = None):
+    session = get_session_by_session_id_helper(session_id)
+    user_id = session["session_userid"]
+    room = get_room(room_id)
+
+    if not is_room_valid(room):
+        raise HTTPException(status_code=404, detail="Room is full or expired")
+
+    creator_id = room[0]
+
+    if creator_id == user_id:
+        raise HTTPException(status_code=404, detail="Can not join your own room")
+
+    join_room_action(room_id, user_id)
+    return {"message": "Successfully joined room",
+            "room_id": room_id,            
+            }
--- server/app/routes/rooms_actions.py
+++ server/app/routes/rooms_actions.py
@@ -1,5 +1,5 @@
 '''
-rooms.py
+rooms_actions.py
 SQL inquieries for rooms
 '''
 from app.config.database import get_connection
@@ -14,6 +14,36 @@ def create_room(creator_id: str):
             cur.execute(
                 "INSERT INTO rooms (creator_id, created_at, expires_at) VALUES (%s, %s, %s) RETURNING id",
                 (creator_id, current_time, expiration_time)
+            )
+            room_id = cur.fetchone()[0]
+        conn.commit()
+    return room_id
+
+def get_room(room_id: str):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "SELECT creator_id, joined_user_id, created_at, expires_at FROM rooms WHERE id = %s",
+                (room_id,)
+            )
+            return cur.fetchone()
+
+def is_room_valid(room = None):
+    if room is None:
+        return False
+    joined_user_id = room[1]
+    if joined_user_id is not None:
+        return False
+    expires_at = room[3]
+    current_time = datetime.now(timezone.utc)
+    return current_time < expires_at
+
+def join_room_action(room_id: str, joiner_id: str):
+    with get_connection() as conn:
+        with conn.cursor() as cur:
+            cur.execute(
+                "UPDATE rooms SET joined_user_id = %s WHERE id = %s RETURNING id",
+                (joiner_id, room_id)
             )
             room_id = cur.fetchone()[0]
         conn.commit()
